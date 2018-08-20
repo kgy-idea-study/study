f2c8a33afc00083a101884e44d4d267b08af8c45
kouguangyuan <2275738566@qq.com>
2018-08-20 15:11:37 +0800

    0820

--- java/AGoodLittleProgram/string/src/StringMethod/StringAllMethod.java
+++ java/AGoodLittleProgram/string/src/StringMethod/StringAllMethod.java
@@ -6,7 +6,8 @@ package StringMethod;
  */
 public class StringAllMethod {
     public static void main(String[] args) {
-        String s = "abcdefghigk呵呵";
+        //String s = "abcdefghigk呵呵";
+        String s = "sss呵呵";
         System.out.println(s.isEmpty());
         System.out.println(s.length());
         System.out.println(s.charAt(1));
