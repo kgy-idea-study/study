91f89b3c7ae190c2950249f65d5e764520eb15a8
kouguangyuan <2275738566@qq.com>
2019-01-09 17:55:10 +0800

    20190109

--- python/project/lianxi/lianxi/lianxi1.py
+++ python/project/lianxi/lianxi/lianxi1.py
@@ -9,3 +9,22 @@ classmates.append('Adam')
 print(classmates)
 classmates.insert(1,'jack')
 classmates.pop()
+
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+
+
+conn.commit()
+
+cursor.execute('select * from user where id=?', ('1',))
+
+values = cursor.fetchall()
+
+print(values)
+cursor.close()
+conn.close()
