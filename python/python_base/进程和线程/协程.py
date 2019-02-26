4d052fa1eb68a3d6bc2a76d58fa9a1ea2c667a64
kouguangyuan <2275738566@qq.com>
2019-02-26 18:02:31 +0800

    20190226

---
+++ python/python_base/进程和线程/协程.py
@@ -0,0 +1,49 @@
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+#c = consumer()
+#produce(c)
+
+import asyncio
+
+@asyncio.coroutine
+def hello():
+    print("hello world")
+    # 异步调用asyncio.sleep(1):
+    r = yield from asyncio.sleep(1)
+    print("Hello again!")
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+# 执行coroutine
+loop.run_until_complete(hello())
+#loop.close()
+
+import threading
+
+@asyncio.coroutine
+def hello1():
+    print('Hello world! (%s)' % threading.currentThread())
+    yield from asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello1(), hello1()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
