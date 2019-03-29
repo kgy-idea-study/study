832b2b145b153870d48d1ad0f6f9dcbbd65bd5f0
kouguangyuan <2275738566@qq.com>
2019-03-29 17:30:05 +0800

    20190329

---
+++ java/spring_cloud/provider/src/main/java/com/example/provider/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.example.provider.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author kouguangyuan
+ * @date 2019/3/18 9:50
+ */
+@RestController
+public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        List<ServiceInstance> instances = client.getInstances("hello-service");
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "Hello World";
+    }
+}
