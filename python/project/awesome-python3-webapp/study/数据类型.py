0eb0d985eaceda3e9dbeccb99364618a9c45b760
kouguangyuan <2275738566@qq.com>
2019-05-17 17:55:20 +0800

    20190517

--- python/project/awesome-python3-webapp/study/数据类型.py
+++ python/project/awesome-python3-webapp/study/数据类型.py
@@ -79,6 +79,81 @@ def main():
     list1.clear()
     print(list1)
 
+    fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+    fruits += ['pitaya', 'pear', 'mango']
+    #循环遍历列表元素
+    for fruit in fruits:
+        print(fruit.title(),end=' ')
+    print()
+    #列表切片
+    fruits2 = fruits[1:4]
+    print(fruits2)
+    # fruit3 = fruits  # 没有复制列表只创建了新的引用
+    # 可以通过完整切片操作来复制列表
+    fruits3 = fruits[:]
+    print(fruits3)
+    fruits4 = fruits[-3:-1]
+    print(fruits4)
+    # 可以通过反向切片操作来获得倒转后的列表的拷贝
+    fruits5 = fruits[::-1]
+    print(fruits5)
+
+    #下面的代码实现了对列表的排序操作。
+    list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+    list2 = sorted(list1)
+    # sorted函数返回列表排序后的拷贝不会修改传入的列表
+    # 函数的设计就应该像sorted函数一样尽可能不产生副作用
+    list3 = sorted(list1, reverse=True)
+    # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+    list4 = sorted(list1, key=len)
+    print(list1)
+    print(list2)
+    print(list3)
+    print(list4)
+    # 给列表对象发出排序消息直接在列表对象上进行排序
+    list1.sort(reverse=True)
+    print(list1)
+
+    #我们还可以使用列表的生成式语法来创建列表，代码如下所示。
+    f = [x for x in range(1,10)]
+    print(f)
+    f = [x + y for x in 'ABCDE' for y in '1234567']
+    print(f)
+    # 用列表的生成表达式语法创建列表容器
+    # 用这种语法创建列表之后元素已经准备就绪所以需要耗费较多的内存空间
+    f = [x ** 2 for x in range(1, 1000)]
+    import sys
+    print(sys.getsizeof(f))  # 查看对象占用内存的字节数
+    #print(f)
+    # 请注意下面的代码创建的不是一个列表而是一个生成器对象
+    # 通过生成器可以获取到数据但它不占用额外的空间存储数据
+    # 每次需要数据的时候就通过内部的运算得到数据(需要花费额外的时间)
+    f = (x ** 2 for x in range(1, 1000))
+    print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间
+    print(f)
+    #for val in f:
+     #   print(val)
+
+    #元组
+    #定义元组
+    t = ('骆昊', 38, True, '四川成都')
+    print(t)
+    # 获取元组中的元素
+    print(t[0])
+    print(t[3])
+
+import os
+import time
+def main2():
+    content = '北京欢迎你为你开天辟地…………'
+    while True:
+        # 清理屏幕上的输出
+        os.system('clear') # os.system('clear')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
 
 if __name__ == '__main__':
-    main()
+    #main()
+    main2()
